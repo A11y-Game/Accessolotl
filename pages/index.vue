8754b16304e28f754383990d0b3ec87d621ad9a7
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2024-03-14 14:09:56 +0100

    A11Y-19: Changed newline to LF and formated everything

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,10 @@
 <template>
-  <div class="text-red-500">
-    hallo, welt
-  </div>
+  <div class="text-red-500">hallo, welt</div>
   <NuxtLink to="asdf">Ein Link</NuxtLink>
 </template>
 
 <script lang="ts">
-export default {}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
