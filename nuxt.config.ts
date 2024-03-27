c6b6b9eb13be137dbeed148244f021433bffd08e
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2024-03-27 11:35:43 +0100

    A11Y-19: Added TailwindCSS Typography

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Typography from '@tailwindcss/typography'
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
@@ -7,6 +9,11 @@ export default defineNuxtConfig({
       link: [
         { rel: "icon", type: "image/svg", href: "/accessolotl.svg" }
       ]
+    }
+  },
+  tailwindcss: {
+    config: {
+      plugins: [Typography()]
     }
   },
 });
