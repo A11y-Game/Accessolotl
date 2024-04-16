167ed2926c6631c971ea0583b3453450dd015d85
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2024-04-16 19:50:51 +0200

    A11Y-19: level layout part 1

--- app.vue
+++ app.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="h-full flex flex-col dark:bg-bg-dark bg-bg-light">
+  <div class="max-h-svh h-svh flex flex-col dark:bg-bg-dark bg-bg-light">
     <header class="grid h-24 place-items-center flex-none">
       <p class="dark:text-white text-black">Header</p>
     </header>
-    <main class="flex-auto ">
+    <main class="flex-1 h-0">
       <NuxtPage />
     </main>
   </div>
--- pages/levels/1.vue
+++ pages/levels/1.vue
@@ -29,58 +29,85 @@ export default {
       );
     },
     screenReader() {
-      return `Axolotls are listed as critically endangered in the wild. Image: ${this.altValue || "axolotl.jpg"}`;
+      return `Image: ${this.altValue || "axolotl.jpg"}`;
     },
   },
 };
 </script>
 <template>
-  <div class="flex h-full flex-row">
-    <div class="flex h-full flex-col flex-auto w-2/3">
-      <div class="flex flex-col justify-stretch">
-        <div class="border-2 text-xl">
-          The image:
+  <div class="flex h-full flex-1 flex-row items-stretch gap-8 px-16 pb-16">
+    <div class="flex flex-[3] flex-col items-stretch gap-8 overflow-y-auto">
+      <div
+        class="flex-none rounded-2xl border-2 p-4 text-center font-heading text-xl font-semibold"
+      >
+        <p>Oh no! The 'alt'-tag is missing!</p>
+      </div>
+      <div
+        class="flex flex-1 flex-row gap-8 rounded-2xl border-2 p-8 pt-6 *:flex *:flex-1 *:flex-col *:items-center *:gap-4"
+      >
+        <div class="">
+          <Icon name="mdi:eye-outline" size="2rem" />
           <img
             src="~/assets/img/axolotl-1.jpg"
             alt="axolotl in an aquarium"
-            rounded
+            class="flex-1 self-stretch rounded-2xl object-cover"
           />
         </div>
 
         <div>
-          <h2 class="font-sansa">What a visually impaired user hears</h2>
-          <p>{{ screenReader }}</p>
-        </div>
-        <div>
-          <h2>What a user with slow internet connection sees</h2>
-          <Icon name="material-symbols:broken-image-rounded" color="gray" />
-          {{ altValue || "" }}
-        </div>
-      </div>
-
-      <div class="border-2">
-          <div class="border-2 font-mono">
-            <div class="whitespace-pre-wrap">{{ before }}</div>
-            <input
-              type="text"
-              v-model="code"
-              autofocus
-              autocapitalize="none"
-              spellcheck="false"
-              class="w-full resize-none bg-amber-100 outline-none transition-colors duration-200"
-              :class="{
-                'bg-green-100': correct,
-              }"
-            />
-            <div class="whitespace-pre-wrap">{{ after }}</div>
+          <Icon name="material-symbols:hearing" size="2rem" />
+          <div
+            class="grid flex-1 place-items-center self-stretch text-pretty rounded-2xl border-2 p-4 text-center"
+          >
+            <p>{{ screenReader }}</p>
           </div>
         </div>
 
+        <div>
+          <Icon name="ic:twotone-wifi-off" size="2rem" />
+
+          <div
+            class="grid flex-1 place-items-center self-stretch rounded-2xl border-2 p-4"
+          >
+            <p>
+              <Icon name="material-symbols:broken-image-rounded" />
+              {{ altValue || "" }}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex flex-none gap-2 rounded-2xl border-2 p-2">
+        <div class="flex-1 rounded-2xl border-2 p-2 font-mono">
+          <div class="whitespace-pre-wrap">{{ before }}</div>
+          <input
+            type="text"
+            v-model="code"
+            autofocus
+            autocapitalize="none"
+            spellcheck="false"
+            class="w-full resize-none bg-amber-100 outline-none transition-colors duration-200"
+            :class="{
+              'bg-green-100': correct,
+            }"
+          />
+          <div class="whitespace-pre-wrap">{{ after }}</div>
+        </div>
+        <div class="flex flex-col justify-center gap-2">
+          <button class="size-12 rounded-lg border-2">
+            <Icon name="lucide:lightbulb" class="size-8" />
+          </button>
+          <button class="size-12 rounded-lg border-2">
+            <Icon name="lucide:rotate-ccw" class="size-7" />
+          </button>
+        </div>
+      </div>
     </div>
 
-  
-    <div class="prose dark:prose-invert rounded border-2 border-accent bg-bg-light p-8 overflow-y-auto text-base flex-auto w-1/3 dark:bg-[#293e74]">
-        <ContentDoc path="/alt-1" />
+    <div
+      class="prose flex-1 overflow-y-auto rounded-2xl border-2 border-accent bg-bg-light p-8 text-base dark:prose-invert dark:bg-[#293e74]"
+    >
+      <ContentDoc path="/alt-1" />
     </div>
   </div>
 </template>
