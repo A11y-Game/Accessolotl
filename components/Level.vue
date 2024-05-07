eb7fac69dcfe8bfa89983e5c78285758bf7181e0
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2024-05-07 21:03:09 +0200

    A11Y-44: Level-Skelett (w/o A11Y) (#15)
    * A11Y-44: first version of level component
    
    * A11Y-44: Level component improvements
    
    * A11Y-44: added single line code editor
    
    * A11Y-44: basic implementation of ImageSeeHearRead
    
    * A11Y-44: Update SingleLineCodeEditor
    is now dumb except for hint, get's its state passed through props
    
    * A11Y-44: implemented layout changed in A11Y-52
    
    * A11Y-44: Completed SingleLineCodeEditor
    
    * A11Y-44: Layout corrections
    
    * A11Y-44: Updated Level 1 to use Level-Components
    
    * A11Y-44: Update Level Number

---
+++ components/Level.vue
@@ -0,0 +1,75 @@
+<script lang="ts">
+import VueMarkdown from "vue-markdown-render";
+
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    markdown: {
+      type: String,
+      required: true,
+    },
+    task: {
+      type: String,
+      required: true,
+    },
+    isCorrect: {
+      type: Boolean,
+      required: true,
+    },
+    levelNumber: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  components: {
+    VueMarkdown,
+  },
+  methods: {
+    nextLevel() {
+      this.$router.push((this.levelNumber + 1).toString());
+    },
+  },
+});
+</script>
+<template>
+  <div class="flex h-full flex-1 flex-row items-stretch gap-8 px-16 pb-12">
+    <div
+      class="flex flex-[3] flex-col items-stretch gap-8 overflow-y-auto pb-3 text-base"
+    >
+      <div
+        class="flex-none rounded-2xl border-2 border-blue-4 bg-blue-3-light p-4 text-center font-heading text-xl font-semibold shadow-large-drop-shadow dark:bg-blue-3-dark"
+      >
+        <p>{{ task }}</p>
+      </div>
+      <div class="flex-1">
+        <slot></slot>
+      </div>
+    </div>
+
+    <div class="flex flex-1 flex-col items-stretch gap-8 pb-3">
+      <div
+        class="prose flex-1 overflow-y-auto rounded-2xl border-2 border-blue-4 bg-blue-3-light p-8 shadow-large-drop-shadow dark:prose-invert first:*:*:mt-0 dark:bg-blue-3-dark"
+      >
+        <VueMarkdown :source="markdown" />
+      </div>
+
+      <button
+        class="flex justify-center rounded-2xl p-1 dark:text-blue-1-dark"
+        :disabled="!isCorrect"
+        @click="nextLevel"
+        :title="isCorrect ? 'Go to next level' : 'Solve the task first'"
+        :class="{
+          'bg-button-active dark:bg-button-active': isCorrect,
+          'cursor-not-allowed bg-button-disabled dark:bg-button-disabled':
+            !isCorrect,
+        }"
+      >
+        <Icon name="ic:round-navigate-next" class="size-12" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<style></style>
