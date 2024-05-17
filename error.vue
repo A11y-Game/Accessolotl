2d9eca1ac673c5bd42cf8e92d8cb4b22eb0504b0
Lisa-Marie <0198@htl.rennweg.at>
2024-05-17 22:02:09 +0200

    A11Y-67 redirecting to homepage when error-alert is closed

---
+++ error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="place-items-center h-screen bg-[url('~/assets/img/error-page-background.svg')] bg-repeat"
+  >
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["error"],
+  layout: "error", // you can set a custom layout for the error page
+  mounted() {
+    this.alertAndRedirect();
+  },
+  methods: {
+    alertAndRedirect() {
+      alert(this.error.message + "\nWe will direct you to the home page.");
+      this.$nuxt.$router.push("/");
+    },
+  },
+};
+</script>
