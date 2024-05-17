b4bca28b9bcb12d08d7ad187bd996ac6ffc9c1d3
Marco Janderka <75068841+Pycxtv@users.noreply.github.com>
2024-05-17 23:57:36 +0200

    Update components/Level.vue

--- components/Level.vue
+++ components/Level.vue
@@ -40,7 +40,7 @@ export default defineComponent({
   watch: {
     isCorrect(newValue) {
       if (newValue) {
-        // const progressStore = useProgressStore();
+        
         this.progressStore.levelUp();
       }
     },
