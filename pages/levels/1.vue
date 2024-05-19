1be7c79eeb38ccf2277b96d73ed68a2941e30852
Marco Janderka <75068841+Pycxtv@users.noreply.github.com>
2024-05-19 16:35:54 +0200

    Switched levels 1 and 2 and corrected levelCount

--- pages/levels/1.vue
+++ pages/levels/1.vue
@@ -1,69 +1,80 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
-const code = `    <img src="axolotl.jpg"></img>`;
+const code = `    <p><span class="axolotlText"></span></p>`;
 
 export default defineComponent({
   data: () => {
     return {
-      markdown: `\`alt\`-tags, what are they for, again? Right, they exist to provide some form of “alternative text”. But what’s that for?
+      markdown: `What's up with images of text? 
 
-It turns out that not everybody can simply look at an image on a webpage to understand what it is about. Visually impaired users use screen-readers, that, well, read out loud what’s on the screen, more or less. And a screen reader – a stupid algorithm – doesn’t understand what the image is about without our help: an \`alt\`-tag.
+You might be tempted to use them but in Web-Accessibility their use is a no-go. While embedding text as images seems like an easy way to style it, it creates problems for users with visual impairments. Screen readers, used by many such users, can't interpret text within images, leaving them unable to access the content. 
 
-\`alt\`-tags not only help the visually impaired: Imagine you’re in the countryside visiting your slightly annoying uncle living in the back of beyond, and your internet connection is unfathomably slow. You might not be able to load the image! But with the help of an \`alt\`-tag you can still understand what the image is about. The same also applies to search engines!
+Also keep in mind that some of these users often rely on enlarging text for better reading, but images are more likely to distort and pixelate when resized. Additionally, options to adjust contrast or font size for better readability don't work with text in images. Translation tools also can't handle text embedded in images, creating another barrier for users who rely on them.
 
 ## Links
-- [Images Tutorial at Web Accessibility Initiative](https://www.w3.org/WAI/tutorials/images/)`,
+- [Images in Text in Images Tutorial](https://www.w3.org/WAI/tutorials/images/textual/)`,
 
       code,
       defaultCode: code,
-      task: "Oh no! The 'alt'-tag is missing!",
+      task: "Oh no! That's not how you use pictures!",
     };
   },
   computed: {
-    /** returns the alt tag value */
-    altValue(): string | undefined {
-      // skip on SSR
-      if (typeof DOMParser === 'undefined') {
-        return undefined;
-      };
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(this.code, "text/html");
-      return doc.querySelector("img")?.getAttribute("alt") ?? undefined;
-    },
     isCorrect(): boolean {
-      return (this.altValue?.includes("axolotl") && !this.altValue?.includes("src")) || false;
+      return /<p>\s*Axo<span class=(('axolotlText')|("axolotlText"))>lotl\s*<\/span>\s*<\/p>/.test(
+        this.code,
+      );
     },
   },
 });
 </script>
-
 <template>
   <Level :markdown :task :isCorrect :levelNumber="1">
     <div class="flex h-full w-full flex-col gap-8">
-      <ImageSeeHearRead filename="axolotl.jpg" :altValue>
-        <div
-          class="axolotlImage flex-1 self-stretch overflow-hidden rounded-2xl bg-cover bg-center shadow-small-drop-shadow"
-          role="img"
-          aria-label="axolotl in an aquarium"
-          title="axolotl in an aquarium"
-        ></div>
-      </ImageSeeHearRead>
-      <SingleLineCodeEditor
-        before="<body>
-    <p>Axolotls are listed as critically endangered in the wild.</p>"
-        :defaultCode="defaultCode"
-        after="</body>"
-        hint="    <--! Add an 'alt'-tag of “axolotl in an aquarium” to the image -->"
-        :isCorrect
-        v-model:currentCode="code"
-      ></SingleLineCodeEditor>
+      <div
+        class="flex flex-1 flex-row gap-8 rounded-2xl border-2 border-blue-4 bg-blue-3-light p-8 pt-6 shadow-small-drop-shadow *:flex *:flex-1 *:flex-col *:items-center *:gap-4 dark:bg-blue-3-dark"
+      >
+        <div title="What a sighted user sees">
+          <Icon name="mdi:image" size="2rem" />
+          <div
+            class="grid flex-1 place-items-center self-stretch text-pretty rounded-2xl bg-blue-5-light p-4 text-center shadow-small-drop-shadow dark:bg-blue-5-dark forced-colors:outline"
+          >
+            <img
+              src="~/assets/img/axolotl-lettering-2-light.svg"
+              alt="text 'axolotl' with the first 3 letters in usual text color and the remaining 4 in dark pink"
+              class="w-full object-cover p-20 dark:hidden"
+            />
+            <img
+              src="~/assets/img/axolotl-lettering-2-dark.svg"
+              alt="text 'axolotl' with the first 3 letters in usual text color and the remaining 4 in light pink"
+              class="hidden w-full object-cover p-20 dark:block"
+            />
+          </div>
+        </div>
+        <div title="What is accessible for the screen reader ">
+          <Icon name="mdi:code" size="2rem" />
+          <div
+            class="grid flex-1 place-items-center self-stretch rounded-2xl bg-blue-5-light p-4 shadow-small-drop-shadow dark:bg-blue-5-dark forced-colors:outline"
+          >
+            <div
+              v-html="code"
+              class="w-full truncate text-center font-heading text-8xl [&_span]:text-axolotl-dark dark:[&_span]:text-axolotl-light"
+            ></div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <SingleLineCodeEditor
+          before='<body>
+    <img src="axolotl-lettering.png" alt="axolotl lettering">'
+          :defaultCode="defaultCode"
+          after="</body>"
+          hint="    <--! Write 'Axo' before the <span>-Tag and 'lotl' inside to color it -->"
+          :isCorrect
+          v-model:currentCode="code"
+        ></SingleLineCodeEditor>
+      </div>
     </div>
   </Level>
 </template>
-
-<style scoped>
-.axolotlImage {
-  background-image: url("~/assets/img/axolotl-1.jpg");
-}
-</style>
