1fee50d3bcc6509a105e1e60d8a5ff36891b0c00
Marco Janderka <75068841+Pycxtv@users.noreply.github.com>
2024-05-25 11:43:05 +0200

    A11Y-74 Implemented changes from PR

--- components/Level.vue
+++ components/Level.vue
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 import VueMarkdown from "vue-markdown-render";
 
 import { defineComponent } from "vue";
@@ -39,12 +39,12 @@ export default defineComponent({
       }
     },
     fireConfetti() {
-      var count = 200;
-      var defaults = {
+      const count = 200;
+      const defaults = {
         origin: { x: 0.85, y: 0.8 },
       };
 
-      function fire(particleRatio, opts) {
+      function fire(particleRatio: number, opts: any) {
         confetti({
           ...defaults,
           ...opts,
@@ -87,10 +87,12 @@ export default defineComponent({
   watch: {
     isCorrect(newValue) {
       if (newValue && this.isLastLevel) {
-        alert(
-          "🎉 Congratulations, you did it! 🎉\nYou've completed all levels! 🥳🎊\n\nDon't forget to refer to the resources on our welcome page to dive even deeper into web accessibility! 📚💻",
-        );
         this.fireConfetti();
+        setTimeout(() => {
+          alert(
+            "🎉 Congratulations, you did it! 🎉\nYou've completed all levels! 🥳🎊\n\nDon't forget to refer to the resources on our welcome page to dive even deeper into web accessibility! 📚💻",
+          );
+        }, 50);
       } else if (newValue) {
         this.progressStore.levelUp();
       }
@@ -101,12 +103,9 @@ export default defineComponent({
 <template>
   <Title>Level {{ levelNumber }}</Title>
   <div class="flex h-full flex-1 flex-row items-stretch gap-8 px-16 pb-16">
-    <div
-      class="flex flex-[3] flex-col items-stretch gap-8 overflow-y-auto pb-3 text-base"
-    >
+    <div class="flex flex-[3] flex-col items-stretch gap-8 overflow-y-auto pb-3 text-base">
       <div
-        class="flex-none rounded-2xl border-2 border-blue-4 bg-blue-3-light p-4 text-center font-heading text-xl font-semibold shadow-large-drop-shadow dark:bg-blue-3-dark"
-      >
+        class="flex-none rounded-2xl border-2 border-blue-4 bg-blue-3-light p-4 text-center font-heading text-xl font-semibold shadow-large-drop-shadow dark:bg-blue-3-dark">
         <p>{{ task }}</p>
       </div>
       <div class="flex-1">
@@ -116,36 +115,25 @@ export default defineComponent({
 
     <div class="flex flex-1 flex-col items-stretch gap-8 pb-3">
       <div
-        class="prose flex-1 overflow-y-auto rounded-2xl border-2 border-blue-4 bg-blue-3-light p-8 shadow-large-drop-shadow dark:prose-invert first:*:*:mt-0 dark:bg-blue-3-dark"
-      >
+        class="prose flex-1 overflow-y-auto rounded-2xl border-2 border-blue-4 bg-blue-3-light p-8 shadow-large-drop-shadow dark:prose-invert first:*:*:mt-0 dark:bg-blue-3-dark">
         <VueMarkdown :source="markdown" />
       </div>
 
       <button
         class="flex justify-center rounded-2xl p-1 hover:opacity-90 active:opacity-60 dark:text-blue-1-dark forced-colors:outline"
-        :disabled="!isCorrect"
-        @click="nextLevel"
-        :title="
-          isLastLevel && isCorrect
-            ? 'Fire confetti!'
-            : isCorrect
-              ? 'Go to next level'
-              : 'Going to next level disallowed: Solve the task first'
-        "
-        :class="{
-          'bg-button-active dark:bg-button-active': isCorrect,
-          'cursor-not-allowed bg-button-disabled dark:bg-button-disabled':
-            !isCorrect,
-        }"
-      >
-        <Icon
-          :name="
-            isLastLevel && isCorrect
-              ? 'noto:party-popper'
-              : 'ic:round-navigate-next'
-          "
-          class="size-12"
-        />
+        :disabled="!isCorrect" @click="nextLevel" :title="isCorrect ? (isLastLevel ? 'Fire confetti!' : 'Go to next level')
+    : 'Going to next level disallowed: Solve the task first'
+    " :class="{
+    'bg-button-active dark:bg-button-active': isCorrect,
+    'cursor-not-allowed bg-button-disabled dark:bg-button-disabled':
+      !isCorrect,
+  }">
+        <Icon :name="isLastLevel
+    ? 'noto:party-popper'
+    : 'ic:round-navigate-next'
+    " class="size-12" :class="{
+    'grayscale opacity-70': !isCorrect && isLastLevel,
+  }" />
       </button>
     </div>
   </div>
